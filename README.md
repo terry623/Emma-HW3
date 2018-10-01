451cb7b445081bc66b31e97509b49a1f1a386551
terry623 <terry92516@gmail.com>
2018-10-01 17:29:14 +0800

    add other function ( empty )

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Emma-HW3
+
+1. 安裝 [Python3](https://www.python.org/downloads/)
+2. 在此目錄下
+
+```sh
+$ pip install -r requirements.txt
+$ python main.py
+```
--- main.py
+++ main.py
@@ -7,19 +7,31 @@ endTime = "2018-10-01"
 
 
 def getsDataFromYahooFinance(company):
+    data = None
     try:
         data = pdr.get_data_yahoo(company, start=startTime, end=endTime)
+        # 輸出成 csv 方便自己查看
         data.to_csv('.\\output\\' + company + '.csv')
     except:
         print('Something Error, Please Try Again!')
+    return data
 
 
 def getVIX(company):
     print('VIX Not Yet!')
 
 
-def calculateVolatility():
+def calculateVolatility(data):
     print('Volatility Not Yet!')
 
 
-getsDataFromYahooFinance("AAPL")
+def getDataAndCalculate(company):
+    companyData = getsDataFromYahooFinance(company)
+    calculateVolatility(companyData)
+
+
+# 從這開始看
+# 填入公司名字
+getDataAndCalculate('AAPL')
+getDataAndCalculate('GOOG')
+getDataAndCalculate('AMZN')
