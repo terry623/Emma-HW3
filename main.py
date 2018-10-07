9cd9d546645b479d845285ee9bc46446d50ddd91
terry623 <terry92516@gmail.com>
2018-10-07 11:41:20 +0800

    change output folder path

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def drawChart(result, company):
     figure = result.plot(x="startDay", y='volatility',
                          title=title)
     picture = figure.set_xlabel("Number of Times").get_figure()
-    picture.savefig('.\\OUTPUT\\' + company + '\\' + company + '.png')
+    picture.savefig('.\\OUTPUT\\' + company + '.png')
 
 
 def getVIX(company):
